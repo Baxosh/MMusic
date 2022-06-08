918bfec5664a6a13c00bc50315d1b6c4c1bdb62c
Bakhodir <bakhodir.2000.19@gmail.com>
2022-06-08 16:12:28 +0500

    First initialized src folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0)
+	return (
+		<div className="App">
+			<h1>Hello, Monday Music project</h1>
+
+			<h1>{count}</h1>
+			<button className="button" onClick={() => setCount(count + 1)}>
+				+
+			</button>
+		</div>
+	)
 }
 
-export default App;
+export default App
